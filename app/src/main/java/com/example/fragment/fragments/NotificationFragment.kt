2cd7128435ee74b33c233e5258dd43e827c5cb69
Vaniko1 <ivane.nozadze.1@btu.edu.ge>
2022-11-28 04:44:50 +0400

    first commit

---
+++ app/src/main/java/com/example/fragment/fragments/NotificationFragment.kt
@@ -0,0 +1,8 @@
+package com.example.fragment.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.fragment.R
+
+class NotificationFragment : Fragment(R.layout.fragment_notification){
+
+}
---
+++ app/src/main/java/com/example/fragment/fragments/newFragment.kt
@@ -0,0 +1,7 @@
+package com.example.fragment.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.fragment.R
+
+class newFragment: Fragment(R.layout.fragment_new) {
+}
